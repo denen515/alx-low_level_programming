43a2032e160fa64112dfef71c7f9316fdc7d2937
Vershima Denen <vdenen@yahoo.com>
2022-09-14 07:47:57 -0700

    ball

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_times_table - prints times table
  * @n : times table to use
@@ -36,6 +35,7 @@ void print_times_table(int n)
 			{
 				_putchar('0' + rep / 100);
 				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
 			}
 			if (b < n)
 			{
