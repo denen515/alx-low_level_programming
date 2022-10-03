f0dd0d130ddad1dc91f95d042f07cc1a29e706b2
Vershima Denen <vdenen@yahoo.com>
2022-10-03 03:55:40 -0700

    r

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,14 +6,14 @@
  * @haystack: main str to be examined
  * @needle: searched in haystack
  * Return: return 0
- */
+ **/
 
 char  *_strstr(char *haystack, char *needle)
 {
 	char *str1, *str2; /*Declaring variables*/
 
 	while (*haystack != '\0')
-	[
+	{
 		str1 = haystack; /*values*/
 		str2 = needle;
 		/*Star WHILE*/
